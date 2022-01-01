87cff0000b0f32760ef7d0f591b5e989be7e2831
Tulio Colmanetti <tulio.colmanetti@gmail.com>
2022-01-01 17:45:57 -0300

    Started to implement controller methods for managing Jogo instance;

--- src/com/rnv/batalhaNaval/controller/BatalhaNaval.java
+++ src/com/rnv/batalhaNaval/controller/BatalhaNaval.java
@@ -1,4 +1,36 @@
 package com.rnv.batalhaNaval.controller;
 
+import com.rnv.batalhaNaval.domain.Jogo;
+import com.rnv.batalhaNaval.domain.Tabuleiro;
+
 public class BatalhaNaval {
+    private Jogo jogo;
+
+    public BatalhaNaval() {
+        this.jogo = new Jogo(null, null);
+    }
+
+    public Jogo getJogo() {
+        return jogo;
+    }
+
+    public void setJogo(Jogo jogo) {
+        this.jogo = jogo;
+    }
+
+    public void prepareJogo() {
+        this.jogo.getJogador().getTabuleiro();
+    }
+
+    public void prepareTabuleiro() {
+        Tabuleiro localTabuleiro = new Tabuleiro();
+        char[][] localGrid = localTabuleiro.getGrid();
+
+        //Adiciona 11 navios (submarinos) em posições aleatórias
+        for (int i = 0; i < 11; i++) {
+
+        }
+
+
+    }
 }
